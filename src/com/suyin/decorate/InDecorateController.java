7658cb885c240e298736164c8bad08937936720b
system9@163.com <qq329009099>
2018-04-21 12:12:39 +0800

    修改名称

---
+++ src/com/suyin/decorate/InDecorateController.java
@@ -0,0 +1,10 @@
+package com.suyin.decorate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/indecorate")
+public class InDecorateController {
+
+}
