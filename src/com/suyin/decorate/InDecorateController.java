63725212b43ecd4937dfd8ae4c102360b04fe099
system9@163.com <qq329009099>
2018-04-21 15:41:09 +0800

    222

--- src/com/suyin/decorate/InDecorateController.java
+++ src/com/suyin/decorate/InDecorateController.java
@@ -3,6 +3,11 @@ package com.suyin.decorate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 营销活动二维码分享
+ * @author Administrator
+ *
+ */
 @Controller
 @RequestMapping("/indecorate")
 public class InDecorateController {
