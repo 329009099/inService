e53c07476b5c2abb3cc4cd5246e85072694b8bba
system9@163.com <qq329009099>
2018-04-22 21:25:07 +0800

    新增金额区间随机
    新增金额区间随机

--- src/com/suyin/common/Utils.java
+++ src/com/suyin/common/Utils.java
@@ -1,10 +1,44 @@
 package com.suyin.common;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.ModelMap;
 
 public class Utils {
+	public static void main(String[] args) throws Exception {
+        double max = 0.08;
+        double min = 1.08;
+        DecimalFormat    df   = new DecimalFormat("######0.00");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(df.format(Utils.nextDouble(min - 1, max + 1)));
+		}
+
+    }
+	/**
+	 * 浮点数 金额 区间随机
+	 * @param min
+	 * @param max
+	 * @return
+	 * @throws Exception
+	 */
+    public static double nextDouble(final double min, final double max) throws Exception {
+        if (max < min) {
+            throw new Exception("min < max");
+        }
+        if (min == max) {
+            return min;
+        }
+        
+        return min + ((max - min) * new Random().nextDouble());
+    }
+    /**
+     * 统一的参数接受
+     * @param request
+     * @param result
+     */
 	public static void fillResult(HttpServletRequest request, ModelMap result) {
 		result.put("module", request.getParameter("module"));
 		result.put("regtype", request.getParameter("regtype"));
