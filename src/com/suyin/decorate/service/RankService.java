146edad47a8308e0045cf2850d7c87884bd2782a
leonardchun <chuncumt@163.com>
2018-04-23 19:52:42 +0800

    rank
    rank

---
+++ src/com/suyin/decorate/service/RankService.java
@@ -0,0 +1,21 @@
+package com.suyin.decorate.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RankService {
+
+	/**
+	 * 通过openid查询名次
+	 * @param openId
+	 * @return
+	 */
+	/*public Integer getMyRankInfo(String openId);*/
+	
+	/**
+	 * 
+	 * @param condition
+	 * @return
+	 */
+	List<Map<String,Object>> findAllRankInfo( Map<String,Object> condition);
+}
---
+++ src/com/suyin/decorate/service/impl/RankServiceImpl.java
@@ -0,0 +1,29 @@
+package com.suyin.decorate.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.suyin.decorate.mapper.RankMapper;
+import com.suyin.decorate.service.RankService;
+
+@Service
+public class RankServiceImpl implements RankService {
+	
+	@Autowired
+	private RankMapper rankMapper;
+
+/*	@Override
+	public Integer getMyRankInfo(String openId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+*/
+	@Override
+	public List<Map<String, Object>> findAllRankInfo(Map<String, Object> condition) {
+		return this.rankMapper.findAllRanListByPage(condition);
+	}
+
+}
